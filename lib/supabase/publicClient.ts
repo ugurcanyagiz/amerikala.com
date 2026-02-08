b3618ce9561a6ca45539f71b9dd733a1214d86b7
ugurcanyagiz <Ugurcanyagiz@gmail.com>
2026-02-08 01:25:54 -0500

    Create publicClient.ts

---
+++ lib/supabase/publicClient.ts
@@ -0,0 +1,18 @@
+import { createBrowserClient } from "@supabase/ssr";
+
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+
+if (!supabaseUrl || !supabaseAnonKey) {
+  throw new Error(
+    "Supabase URL veya Anon Key bulunamadı. Lütfen .env.local dosyasını kontrol edin."
+  );
+}
+
+export const publicSupabase = createBrowserClient(supabaseUrl, supabaseAnonKey, {
+  auth: {
+    persistSession: false,
+    autoRefreshToken: false,
+    detectSessionInUrl: false,
+  },
+});
